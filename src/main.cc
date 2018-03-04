4c232341716692ac054a96097f4110bd1cae5e40
Junhao Li <streaver91@gmail.com>
2018-03-03 21:05:43 -0500

    .

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,21 @@
 #include <cstdio>
+#include <ctime>
 #include "config.h"
+#include "parallel.h"
 
 int main() {
-  printf("Semistochastic Heat-bath Configuration Interation (SHCI)\n");
+  const int proc_id = Parallel::get_proc_id();
+
+  if (proc_id == 0) {
+    printf("Semistochastic Heat-bath Configuration Interation (SHCI)\n\n");
+    const time_t start_time = time(0);
+    printf("Start time: %s", asctime(localtime(&start_time)));
+    const int n_procs = Parallel::get_n_procs();
+    const int n_threads = Parallel::get_n_threads();
+    printf("Infrastructure: %d nodes * %d threads\n", n_procs, n_threads);
+    printf("Configuration:\n");
+    Config::print();
+  }
 
   return 0;
 }
