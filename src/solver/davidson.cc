b5afcc475a246aadd97e43a39919c5ff31272d7c
Junhao Li <streaver91@gmail.com>
2018-06-01 10:27:41 -0400

    .

--- src/solver/davidson.cc
+++ src/solver/davidson.cc
@@ -31,36 +31,17 @@ void Davidson::diagonalize(
   std::vector<double> Hw(dim);
   for (size_t i = 0; i < n_iterations_store; i++) {
     v[i].resize(dim);
-    Hv[i].resize(dim);
   }
-  // Eigen::MatrixXd v = Eigen::MatrixXd::Zero(dim, n_iterations_store);
   double norm = sqrt(Util::dot_omp(initial_vector, initial_vector));
 #pragma omp parallel for
   for (size_t j = 0; j < dim; j++) v[0][j] = initial_vector[j] / norm;
-  // v.col(0).normalize();
 
-  // Eigen::MatrixXd Hv = Eigen::MatrixXd::Zero(dim, n_iterations_store);
-  // Eigen::VectorXd w = Eigen::VectorXd::Zero(dim);
-  // Eigen::VectorXd Hw = Eigen::VectorXd::Zero(dim);
   Eigen::MatrixXd h_krylov = Eigen::MatrixXd::Zero(n_iterations_store, n_iterations_store);
   std::vector<double> eigenvector_krylov(n_iterations_store);
-  // Eigen::VectorXd eigenvalues = Eigen::VectorXd::Zero(n_iterations_store);
   converged = false;
-  // std::vector<double> tmp_v(dim);
-  // Get diagonal elements.
-  // Eigen::VectorXd diag_elems(dim);
-  // for (size_t i = 0; i < dim; i++) diag_elems[i] = matrix.get_diag(i);
-
-  // First iteration.
-  // for (size_t i = 0; i < dim; i++) tmp_v[i] = v(i, 0);
-  // const auto& tmp_Hv = matrix.mul(tmp_v);
   Hv[0] = matrix.mul(v[0]);
-  // for (size_t i = 0; i < dim; i++) Hv(i, 0) = tmp_Hv[i];
-  // lowest_eigenvalue = v.col(0).dot(Hv.col(0));
   lowest_eigenvalue = Util::dot_omp(v[0], Hv[0]);
   h_krylov(0, 0) = lowest_eigenvalue;
-  // w = v.col(0);
-  // Hw = Hv.col(0);
   w = v[0];
   Hw = Hv[0];
   if (verbose) printf("Davidson #0: %.10f\n", lowest_eigenvalue);
@@ -69,9 +50,6 @@ void Davidson::diagonalize(
   for (size_t it = 1; it < n_iterations_store * 2; it++) {
     size_t it_circ = it % n_iterations_store;
     if (it >= n_iterations_store && it_circ == 0) {
-      // v.col(0) = w.col(0);
-      // Hv.col(0) = Hw.col(0);
-      // lowest_eigenvalue = v.col(0).dot(Hv.col(0));
       v[0] = w;
       Hv[0] = Hw;
       lowest_eigenvalue = Util::dot_omp(v[0], Hv[0]);
@@ -83,46 +61,29 @@ void Davidson::diagonalize(
     for (size_t j = 0; j < dim; j++) {
       const double diff_to_diag = lowest_eigenvalue - matrix.get_diag(j);  // diag_elems[j];
       if (std::abs(diff_to_diag) < 1.0e-12) {
-        // v(j, it_circ) = (Hw(j, 0) - lowest_eigenvalue * w(j, 0)) / -1.0e-12;
         v[it_circ][j] = (Hw[j] - lowest_eigenvalue * w[j]) / -1.0e-12;
       } else {
-        // v(j, it_circ) = (Hw(j, 0) - lowest_eigenvalue * w(j, 0)) / diff_to_diag;
         v[it_circ][j] = (Hw[j] - lowest_eigenvalue * w[j]) / diff_to_diag;
       }
     }
 
     // Orthogonalize and normalize.
     for (size_t i = 0; i < it_circ; i++) {
-      //       double prod = 0.0;
-      // #pragma omp parallel for reduction(+ : prod)
-      //       for (size_t j = 0; j < dim; j++) {
-      //         prod += v[it_circ][j] * v[i][j];
-      //       }
       norm = Util::dot_omp(v[it_circ], v[i]);
-// double norm = v.col(it_circ).dot(v.col(i));
 #pragma omp parallel for
       for (size_t j = 0; j < dim; j++) {
-        // v[it_circ][j] -= prod * v[i][j];
         v[it_circ][j] -= norm * v[i][j];
       }
-      // v.col(it_circ) -= norm * v.col(i);
     }
     norm = sqrt(Util::dot_omp(v[it_circ], v[it_circ]));
 #pragma omp parallel for
     for (size_t j = 0; j < dim; j++) {
       v[it_circ][j] /= norm;
     }
-    // v.col(it_circ).normalize();
-
-    // Apply H once.
-    //   for (size_t i = 0; i < dim; i++) tmp_v[i] = v(i, it_circ);
-    //   const auto& tmp_Hv2 = matrix.mul(tmp_v);
     Hv[it_circ] = matrix.mul(v[it_circ]);
-    //   for (size_t i = 0; i < dim; i++) Hv(i, it_circ) = tmp_Hv2[i];
 
     // Construct subspace matrix.
     for (size_t i = 0; i <= it_circ; i++) {
-      // h_krylov(i, it_circ) = v.col(i).dot(Hv.col(it_circ));
       h_krylov(i, it_circ) = Util::dot_omp(v[i], Hv[it_circ]);
       h_krylov(it_circ, i) = h_krylov(i, it_circ);
     }
@@ -130,9 +91,6 @@ void Davidson::diagonalize(
     // Diagonalize subspace matrix.
     Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eigenSolver(
         h_krylov.leftCols(it_circ + 1).topRows(it_circ + 1));
-    //   const auto& eigenvalues = eigenSolver.eigenvalues();
-    //   auto eigenvectors = eigenSolver.eigenvectors();
-    //   lowest_eigenvalue = eigenvalues(0);
     lowest_eigenvalue = eigenSolver.eigenvalues()(0);
     const auto& eigenvectors = eigenSolver.eigenvectors();
     double factor = 1.0;
@@ -147,9 +105,6 @@ void Davidson::diagonalize(
         Hw[j] += Hv[i][j] * eigenvector_krylov[i];
       }
     }
-    //   if (eigenvectors(0, 0) < 0) eigenvectors.col(0) *= -1;
-    //   w = v.leftCols(it_circ + 1) * eigenvectors.col(0).topRows(it_circ + 1);
-    //   Hw = Hv.leftCols(it_circ + 1) * eigenvectors.col(0).topRows(it_circ + 1);
 
     if (verbose) printf("Davidson #%zu: %.10f\n", it_real, lowest_eigenvalue);
     it_real++;
@@ -162,7 +117,4 @@ void Davidson::diagonalize(
     if (converged) break;
   }
   lowest_eigenvector = w;
-  //  lowest_eigenvector[i] = w[i];
-  // #pragma omp parallel for
-  //   for (size_t i = 0; i < dim; i++) lowest_eigenvector[i] = w[i];
 }
