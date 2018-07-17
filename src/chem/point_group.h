49b7272da36018baa525c70c2b1f06e1c585d1a3
Cyrus Umrigar <CyrusUmrigar@cornell.edu>
2018-07-17 14:10:02 -0400

    .

--- src/chem/point_group.h
+++ src/chem/point_group.h
@@ -1,3 +1,3 @@
 #pragma once
 
-enum class PointGroup { C1v, D2h, C2v, Dooh };
+enum class PointGroup { C1, Cs, Ci, C2v, C2h, D2h, Dooh };
--- src/chem/product_table.h
+++ src/chem/product_table.h
@@ -5,7 +5,7 @@
 
 class ProductTable {
  public:
-  ProductTable() : point_group(PointGroup::C1v) {}
+  ProductTable() : point_group() {}
 
   unsigned get_product(const unsigned a, const unsigned b) const;
 
