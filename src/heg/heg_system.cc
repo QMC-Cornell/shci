39aae88e2c067b6562a3f2a64cda05d12636cbb0
Junhao Li <streaver91@gmail.com>
2018-09-10 22:15:53 -0400

    .

--- src/heg/heg_system.cc
+++ src/heg/heg_system.cc
@@ -211,7 +211,6 @@ double HegSystem::get_hamiltonian_elem_no_time_sym(
     if (diff_up.n_diffs > 2) return 0.0;
     diff_dn = det_i.dn.diff(det_j.dn);
     n_excite = diff_up.n_diffs + diff_dn.n_diffs;
-    if (n_excite != 2) return 0.0;
   } else if (n_excite > 0) {
     diff_up = det_i.up.diff(det_j.up);
     if (diff_up.n_diffs < static_cast<unsigned>(n_excite)) {
@@ -225,9 +224,11 @@ double HegSystem::get_hamiltonian_elem_no_time_sym(
     return one_body_energy + two_body_energy;
   } else if (n_excite == 2) {
     return get_two_body_double(diff_up, diff_dn);
+  } else {
+    return 0.0;
   }
 
-  throw new std::runtime_error("Calling hamiltonian with >2 exicitation");
+  // throw new std::runtime_error("Calling hamiltonian with >2 exicitation");
 }
 
 double HegSystem::get_one_body_diag(const Det& det) const {
